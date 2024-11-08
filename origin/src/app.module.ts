c1f69326bb22d586d514955456f1efa039b08f93
Thy Tran <22521461@gm.uit.edu.vn>
2024-11-08 16:08:51 +0700

    Setup MinIO

--- origin/src/app.module.ts
+++ origin/src/app.module.ts
@@ -1,8 +1,22 @@
 import { Module } from '@nestjs/common';
 import { SharedModule } from './module/shared.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { NestMinioModule } from 'nestjs-minio';
 
 @Module({
-  imports: [SharedModule,
-  ],
+  imports: [
+    SharedModule, 
+    ConfigModule.forRoot({ isGlobal: true }),
+    NestMinioModule.registerAsync({
+      imports: [ConfigModule],
+      useFactory: async (configService: ConfigService) => ({
+        endPoint: configService.get<string>('S3_ENDPOINT'),
+        accessKey: configService.get<string>('S3_ACCESS_KEY'),
+        secretKey: configService.get<string>('S3_SECRET_KEY'),
+        useSSL: configService.get<boolean>('S3_USE_SSL'),
+      }),  
+      inject: [ConfigService],
+    })
+  ]  
 })
 export class AppModule {}
--- origin/src/module/shared.module.ts
+++ origin/src/module/shared.module.ts
@@ -1,9 +1,10 @@
 import { Module } from "@nestjs/common";
 import { FileModule } from "./file/file.module";
 import { RootModule } from './root/root.module';
+import { MinioStorageModule } from './minio-storage/minio-storage.module';
 
 @Module({
-  imports: [FileModule, RootModule],
+  imports: [FileModule, RootModule, MinioStorageModule],
   exports: [FileModule, RootModule],
 })
 export class SharedModule {}
