b7dbd45ab36434e469c76faa304faf26011f26c2
Thy Tran <22521461@gm.uit.edu.vn>
2024-11-14 14:17:33 +0700

    feat: CDN's cache header

--- origin/src/module/file/file.controller.ts
+++ origin/src/module/file/file.controller.ts
@@ -1,12 +1,8 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseInterceptors, UploadedFile, ParseFilePipe, MaxFileSizeValidator, FileTypeValidator, Res, Query } from '@nestjs/common';
+import { Controller, Get, Param, Res, Query } from '@nestjs/common';
 import { FileService } from './file.service';
 import { Response } from 'express';
-import { CreateFileDto } from './dto/create-file.dto';
-import { UpdateFileDto } from './dto/update-file.dto';
 import { END_POINTS } from 'src/utils/endPoints';
-import { FileInterceptor } from '@nestjs/platform-express';
-import { extname, join } from 'path';
-import { diskStorage } from 'multer';
+import { join } from 'path';
 import { ResponseObject } from 'src/utils/responseObject';
 
 @Controller(END_POINTS.FILE.BASE)
@@ -23,53 +19,14 @@ export class FileController {
   @Get(END_POINTS.FILE.DOWNLOAD)
   async download(@Param('key') key: string, @Res() res: Response) {
     const filePath = join(__dirname, '..', '..', '..', 'storage', key);
-    res.header('Cache-Control', 'public, max-age=100');
+    res.header('CDN-Cache-Control', 'public, max-age=100');
     await this.fileService.get(key, filePath);
     
     res.download(filePath);
-    // try {
-    //   const { dataStream, size } = await this.fileService.get(key);
-
-    //   res.set({
-    //     'Content-Type': 'application/octet-stream',
-    //     'Content-Disposition': `attachment; filename="${key}"`,
-    //     'Content-Length': size, 
-    //   });
-
-    //   dataStream.pipe(res);
-
-    //   dataStream.on('error', (err) => {
-    //     console.error('Error streaming file:', err);
-    //     res.status(500).send('Error retrieving file');
-    //   });
-    // } catch (error) {
-    //   console.error('Error fetching object:', error);
-    //   res.status(500).send('Error fetching object');
-    // }
   }
 }
   
-  // @Post(END_POINTS.FILE.UPLOAD)
-  // @UseInterceptors(FileInterceptor('file', {
-  //   storage: diskStorage({
-  //     destination: join(__dirname, '..', '..', '..', 'storage'),
-  //     filename: (req, file, callback) => {
-  //       const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1e9);
-  //       callback(null, file.fieldname + '-' + uniqueSuffix + extname(file.originalname));
-  //     },
-  //   }),
-  // }))
-  // upload(@UploadedFile() file: Express.Multer.File,) {
-  //   console.log(file);
-  //   return { message: 'File uploaded successfully', filePath: file.path };
-  // }
-
-  // @Get(END_POINTS.FILE.DOWNLOAD)
-  // download(@Param('key') key: string, @Res() res: Response) {
-  //   const filePath = join(__dirname, '..', '..', '..', 'storage', key);
-  //   res.header('Cache-Control', 'public, max-age=100');
-  //   res.download(filePath);
-  // }
+  
 
 
 
